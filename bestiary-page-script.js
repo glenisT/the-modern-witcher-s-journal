9f3d5b007a187281236d657571c58137aee04c05
User <glentivari@gmail.com>
2021-08-03 17:24:26 +0200

    Made home page responsive, working on bestiary page

---
+++ bestiary-page-script.js
@@ -0,0 +1,22 @@
+function beastsClicked()
+{
+    //var audio = document.getElementById('source');
+    console.log("CLICKED BEASTS!");
+    //audio.play();
+    //window.open("file:///C:/Users/user/AngularProjects/The%20ModernWitcher's%20Journal/bestiary-page.html");
+}
+
+function cursedClicked()
+{
+    console.log("CLICKED THE CURSED!");
+}
+
+function draconoidsClicked()
+{
+    console.log("CLICKED DRACONOIDS!");
+}
+
+function elementaClicked()
+{
+    console.log("CLICKED ELEMENTA!");
+}
---
+++ main-page-script.js
@@ -0,0 +1,24 @@
+
+
+function bestiaryClicked()
+{
+    //var audio = document.getElementById('source');
+    console.log("CLICKED BESTIARY!");
+    //audio.play();
+    window.open("file:///C:/Users/user/AngularProjects/The%20ModernWitcher's%20Journal/bestiary-page.html");
+}
+
+function signsClicked()
+{
+    console.log("CLICKED THE SIGNS!");
+}
+
+function alchemyClicked()
+{
+    console.log("CLICKED ALCHEMY!");
+}
+
+function mapClicked()
+{
+    console.log("CLICKED MAP!");
+}
