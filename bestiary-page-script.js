72e1cd6a2357356d86208fed5b7e9bbe520ec4b9
User <glentivari@gmail.com>
2021-08-10 12:51:50 +0200

    Add animations to both pages

--- bestiary-page-script.js
+++ bestiary-page-script.js
@@ -20,3 +20,41 @@ function elementaClicked()
 {
     console.log("CLICKED ELEMENTA!");
 }
+
+/*function animateCards()
+{
+    let gridArray = new Array();
+    gridArray = [document.getElementById("beasts"), document.getElementById("cursed-ones"), 
+                document.getElementById("draconids"), document.getElementById("elementa"),
+                document.getElementById("hybrids"), document.getElementById("insectoids"),
+                document.getElementById("necrophages"), document.getElementById("ogroids"),
+                document.getElementById("relicts"), document.getElementById("specters"),
+                document.getElementById("vampires"), document.getElementById("legends")];
+    var i = 0;
+    for(i = 0; i < 12; i++)
+    {
+        gridArray[i].style.animation = 
+    }
+}
+
+function sleep(milliseconds)
+{
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    }
+}
+
+function loadText()
+{
+    var i;
+    let text = new String();
+    text = document.getElementById("beasts").innerHTML;
+    for(i = 0; i < text.length; i++)
+    {
+        document.getElementById("draconids").innerHTML = text.charAt(i);
+        sleep(200);
+    }
+}*/
