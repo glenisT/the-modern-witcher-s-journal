b53700ceb6cb9315ba60ae7dd885ab315b3e2e6b
g.tivari <g.tivari@sogetel.it>
2021-10-06 17:43:06 +0200

    Add basic js idea

---
+++ map-page/map-page.js
@@ -0,0 +1,6 @@
+const container = document.getElementById('container');
+
+container.addEventListener("wheel", function(){
+    container.style.height = "2000px";
+    container.style.width = "5000px";
+});
