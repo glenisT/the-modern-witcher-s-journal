b2f3e1982fdfa9ea9dc28b8b67aff4aead5212a2
g.tivari <g.tivari@sogetel.it>
2021-10-07 14:49:34 +0200

    Create alchemy page

--- map-page/map-page.js
+++ map-page/map-page.js
@@ -1,6 +1,5 @@
 const container = document.getElementById('container');
 
 container.addEventListener("wheel", function(){
-    container.style.height = "2000px";
-    container.style.width = "5000px";
+    container.style.zoom = "80%";
 });
