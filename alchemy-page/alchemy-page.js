686bc382f184c5c79b5f2910db39284dd1c9d48d
g.tivari <g.tivari@sogetel.it>
2021-10-08 18:09:07 +0200

    Add animationend event listener to js

--- alchemy-page/alchemy-page.js
+++ alchemy-page/alchemy-page.js
@@ -16,49 +16,77 @@ function lightUpElements(elementList)
 {
     for(i = 0; i < elementList.length; i++)
     {
+        
         if(elementList[i] == 'vitriol')
         {
             this.vitriol.style.animation = "vitriolShine 0.5s alternate 6";
+            this.vitriol.addEventListener('animationend', ()=>{
+                this.vitriol.removeAttribute('style');
+              });
         }
 
         if(elementList[i] == 'rebis')
         {
             this.rebis.style.animation = "rebisShine 0.5s alternate 6";
+            this.rebis.addEventListener('animationend', ()=>{
+                this.rebis.removeAttribute('style');
+              });
         }
 
         if(elementList[i] == 'hydragenum')
         {
             this.hydragenum.style.animation = "hydragenumShine 0.5s alternate 6";
+            this.hydragenum.addEventListener('animationend', ()=>{
+                this.hydragenum.removeAttribute('style');
+              });
         }
 
         if(elementList[i] == 'quebirth')
         {
             this.quebirth.style.animation = "quebirthShine 0.5s alternate 6";
+            this.quebirth.addEventListener('animationend', ()=>{
+                this.quebirth.removeAttribute('style');
+              });
         }
 
         if(elementList[i] == 'vermilion')
         {
             this.vermilion.style.animation = "vermilionShine 0.5s alternate 6";
+            this.vermilion.addEventListener('animationend', ()=>{
+                this.vermilion.removeAttribute('style');
+              });
         }
 
         if(elementList[i] == 'albedo')
         {
             this.albedo.style.animation = "albedoShine 0.5s alternate 6";
+            this.albedo.addEventListener('animationend', ()=>{
+                this.albedo.removeAttribute('style');
+              });
         }
 
         if(elementList[i] == 'nigredo')
         {
             this.nigredo.style.animation = "nigredoShine 0.5s alternate 6";
+            this.nigredo.addEventListener('animationend', ()=>{
+                this.nigredo.removeAttribute('style');
+              });
         }
 
         if(elementList[i] == 'rubedo')
         {
             this.rubedo.style.animation = "rubedoShine 0.5s alternate 6";
+            this.rubedo.addEventListener('animationend', ()=>{
+                this.rubedo.removeAttribute('style');
+              });
         }
 
         if(elementList[i] == 'aether')
         {
             this.aether.style.animation = "aetherShine 0.5s alternate 6";
+            this.aether.addEventListener('animationend', ()=>{
+                this.aether.removeAttribute('style');
+              });
         }
     }
 }
